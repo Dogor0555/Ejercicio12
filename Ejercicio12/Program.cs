995d0042a66f3d8fd3c418d3ebaa4d843a77bb83
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 06:13:05 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+Console.Write("Ingrese la calificación del estudiante (entre 0 y 100): ");
+int calificacion = int.Parse(Console.ReadLine());
+
+switch (calificacion)
+{
+    case int n when (n >= 90 && n <= 100):
+        Console.WriteLine("Calificación: A");
+        break;
+    case int n when (n >= 80 && n < 90):
+        Console.WriteLine("Calificación: B");
+        break;
+    case int n when (n >= 70 && n < 80):
+        Console.WriteLine("Calificación: C");
+        break;
+    case int n when (n >= 60 && n < 70):
+        Console.WriteLine("Calificación: D");
+        break;
+    case int n when (n < 60 && n >= 0):
+        Console.WriteLine("Calificación: F");
+        break;
+    default:
+        Console.WriteLine("Calificación inválida. Ingrese un valor entre 0 y 100.");
+        break;
+}
